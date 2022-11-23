9cf13f5dd99de3efbcaba1939862e496590158c3
Sahinshf <shahinrsha@code.edu.az>
2022-11-23 09:17:08 +0400

    Task is here

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _23_11_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string str = Console.ReadLine().ToLowerInvariant();
+            
+            
+            if (str[0] != ' ')
+                {
+                    Console.Write(char.ToUpper(str[0]));
+                }
+
+            for (int i = 1; i < str.Length ; i++)
+            {
+
+                if (str[i - 1] == ' ' && str[i] != ' ')
+                {
+
+                    Console.Write(char.ToUpper(str[i]) );
+                }
+                else
+                {
+                    Console.Write(str[i]);
+                }
+            }
+
+        }
+    }
+}
